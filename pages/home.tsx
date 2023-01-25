961c9bb72884d0fe69d39a240023e7a1cb658200
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-25 22:43:33 +0330

    Add Charts

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { ageData, pieData, stackedBarData, lineData } from "./data";
 
 import { PublicContext } from "../../core/context";
 import {
@@ -10,13 +9,15 @@ import {
   Label,
   Divider,
   Media,
-  PieChart,
-  StackedBarChart,
-  BarChart,
-  LineChart,
 } from "../../core/components";
-import { Background, Colors } from "../../core/theme";
-import { TopPlayers, Groups } from "../../market/components";
+import {
+  TopPlayers,
+  Groups,
+  ChartCntPostPlayers,
+  ChartAvgAges,
+  ChartCntSales,
+  ChartPriceChange,
+} from "../../market/components";
 
 export default function HomePage() {
   const { publicCtx } = React.useContext(PublicContext);
@@ -69,35 +70,18 @@ export default function HomePage() {
             </GridHeader>
           </GridItem>
 
-          <CardDashboard
-            title={t("cnt-sale-cards", { ns: "market" })}
-            lg={4}
-            md={6}
-            sm={12}
-          >
-            <StackedBarChart
-              data={stackedBarData}
-              fill={
-                Colors[publicCtx.theme.color][publicCtx.theme.background.name]
-                  .primary
-              }
-            />
-          </CardDashboard>
-          <CardDashboard title={t("cnt-post-players", { ns: "market" })} lg={4} md={6} sm={12}>
-            <PieChart data={pieData} />
-          </CardDashboard>
-          <CardDashboard title={t("avg-age-players", { ns: "market" })} lg={4} md={6} sm={12}>
-            <BarChart data={ageData} />
-          </CardDashboard>
-          <CardDashboard title={t("avg-age-players", { ns: "market" })} lg={8} md={12} sm={12}>
-            <LineChart
-              data={lineData}
-              color={
-                Colors[publicCtx.theme.color][publicCtx.theme.background.name]
-                  .primary
-              }
-            />
-          </CardDashboard>
+          <GridItem lg={4} md={6} sm={12} sx={{ height: "100%" }}>
+            <ChartCntSales />
+          </GridItem>
+          <GridItem lg={4} md={6} sm={12} sx={{ height: "100%" }}>
+            <ChartCntPostPlayers />
+          </GridItem>
+          <GridItem lg={4} md={6} sm={12} sx={{ height: "100%" }}>
+            <ChartAvgAges />
+          </GridItem>
+          <GridItem lg={8} md={12} sx={{ height: "100%" }}>
+            <ChartPriceChange />
+          </GridItem>
 
           <Divider sx={{ mt: 5, mb: 5 }} />
 
@@ -112,29 +96,5 @@ export default function HomePage() {
         </GridHeader>
       </Box>
     </>
-  );
-}
-
-function CardDashboard({ children, title, lg, md, sm }: any) {
-  const { publicCtx } = React.useContext(PublicContext);
-
-  return (
-    <GridItem lg={lg} md={md} sm={sm} sx={{ height: "100%" }}>
-      <>
-        <div
-          style={{
-            height: "350px",
-            margin: "5px",
-            borderRadius: "25px",
-            background: Background[publicCtx.theme.background.name].tertiary,
-          }}
-        >
-          {children}
-        </div>
-        <Label size="p" sx={{ pb: 3 }}>
-          {title}
-        </Label>
-      </>
-    </GridItem>
   );
 }
